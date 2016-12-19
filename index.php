260d2e79b2d3ccd7f7c0735376e9e0123c047c3e
kaileighrose <kaileighrose@gmail.com>
2016-12-18 22:39:29 -0500

    clone down repository and create index.php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+$data = file_get_contents('https://www.codeschool.com/users/kaileighrose.json');
+$json_data = json_decode($data, true);
+//var_dump($data);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+</body>
+</html>
